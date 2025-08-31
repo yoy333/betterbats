c40be9891a108b54218a23ddb160b3b39d3c6d98
Colin Doherty <colinjdoherty@outlook.com>
2025-08-31 08:29:40 -0700

    First Commit

---
+++ src/main/java/com/outlook/colinjdoherty/betterbats/ExampleMod.java
@@ -0,0 +1,50 @@
+package com.outlook.colinjdoherty.betterbats;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(ExampleMod.MOD_ID)
+public class ExampleMod{
+    public static final String MOD_ID = "betterbats";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public ExampleMod(FMLJavaModLoadingContext context){
+        IEventBus modEventBus = context.getModEventBus();
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+        modEventBus.addListener(this::addCreative);
+
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event){
+
+    }
+
+    private void addCreative(BuildCreativeModeTabContentsEvent event){
+
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event){
+
+    }
+
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents{
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event){
+
+        }
+    }
+}
